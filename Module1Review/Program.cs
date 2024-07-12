35567045d048a3782979a79194da93941d02b725
StewartBaker <bakrstewart16@gmail.com>
2024-07-12 15:44:42 -0500

    finished circle switch

--- Module1Review/Program.cs
+++ Module1Review/Program.cs
@@ -31,65 +31,87 @@ class Program
     //Used to call methods and use them
     static void Main(string[] args)
     {
-        //Used to figure out which shape the user wants the area of and stores it in variable
-        Console.WriteLine("What would you like the area of, Square, Rectangle, Triangle or Circle?");
-        var shape = Console.ReadLine();
-
-        //Puts user input in lowercase and then executes code depending on what matches, if none default output
-        switch (shape.ToLower())
+        //bool used to loop through code if an invalid shape is given so user doesnt have to press play again
+        bool finishArea = false;
+        while (!finishArea)
         {
-            case "circle":
-                bool finishCircle = false;
-                while (!finishCircle)
-                {
-                    Console.WriteLine("What is the Radius of your Circle?");
-                    
-                }
-                
-                break;
-            
-            //Creates case for input of triangle
-            case "triangle":
-                //uses bool to loop through code when an invalid integer is given.
-                bool finishTriangle = false;
-                
-                while (!finishTriangle)
-                {
-                    //Asks user for length and stores it
-                    Console.WriteLine("What is the Length of your Triangle?");
-                    string lengthInput = Console.ReadLine();
-                    
-                    //Asks user for height and stores it
-                    Console.WriteLine("What is the Height of your Triangle?");
-                    string heightInput = Console.ReadLine();
+            //Used to figure out which shape the user wants the area of and stores it in variable
+            Console.WriteLine("What would you like the area of, Square, Rectangle, Triangle or Circle?");
+            var shape = Console.ReadLine();
 
-                    //TryParse to make sure both inputs are an integer, if not, prints else statement and loops back for new input
-                    if (double.TryParse(lengthInput, out double baseLength) &&
-                        double.TryParse(heightInput, out double height))
+
+            //Puts user input in lowercase and then executes code depending on what matches, if none default output
+            switch (shape.ToLower())
+            {
+                //creates case for circle input
+                case "circle":
+                    bool finishCircle = false;
+                    while (!finishCircle)
                     {
-                        //output given when integers are valid which calls the method
-                        Console.WriteLine($"The are of a triangle with the baseLength of {baseLength} and height of {height} is {AreaOfTriangle(baseLength, height)}");
-                        //sets bool to true, exiting loop.
-                        finishTriangle = true;
+                        //Asks for radius and stores it
+                        Console.WriteLine("What is the Radius of your Circle?");
+                        string radiusInput = Console.ReadLine();
+
+                        if (double.TryParse(radiusInput, out double radius))
+                        {
+                            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}");
+                            finishCircle = true;
+                        }
+                        else
+                        {
+                            //output when input isnt valid
+                            Console.WriteLine("Invalid Input. Please input a Valid Integer");
+                        }
                     }
-                    else
+
+                    //bool is set to true to exit shape loop
+                    finishArea = true;
+                    break;
+
+                    //Creates case for triangle input
+                    case "triangle":
+                    //uses bool to loop through code when an invalid integer is given.
+                    bool finishTriangle = false;
+
+                    while (!finishTriangle)
                     {
-                        //output when input isnt valid
-                        Console.WriteLine("Invalid Input. Please input a Valid Integer");
+                        //Asks user for length and stores it
+                        Console.WriteLine("What is the Length of your Triangle?");
+                        string baseLengthInput = Console.ReadLine();
+
+                        //Asks user for height and stores it
+                        Console.WriteLine("What is the Height of your Triangle?");
+                        string heightInput = Console.ReadLine();
+
+                        //TryParse to make sure both inputs are an integer, if not, prints else statement and loops back for new input
+                        if (double.TryParse(baseLengthInput, out double baseLength) &&
+                            double.TryParse(heightInput, out double height))
+                        {
+                            //output given when integers are valid which calls the method
+                            Console.WriteLine($"The are of a triangle with the baseLength of {baseLength} and height of {height} is {AreaOfTriangle(baseLength, height)}");
+                            //sets bool to true, exiting loop.
+                            finishTriangle = true;
+                        }
+                        else
+                        {
+                            //output when input isnt valid
+                            Console.WriteLine("Invalid Input. Please input a Valid Integer");
+                        }
                     }
-                }
-                break;
-            
-            case "rectangle":
 
-                break;
-            case "square":
+                    break;
 
-                break;
-            //default output if user input does not match any shapes
-            default:
-                Console.WriteLine("Invalid Shape, Please pick a valid shape.");
-                break;
+                case "rectangle":
+
+                    break;
+                case "square":
+
+                    break;
+                //default output if user input does not match any shapes
+                default:
+                    Console.WriteLine("Invalid Shape, Please pick a valid shape.");
+                    break;
+            }
         }
         //Code for Triangle
         
