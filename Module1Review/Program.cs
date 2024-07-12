fa08168f5f96ba00f9bb8fcb35860a10900d127a
StewartBaker <bakrstewart16@gmail.com>
2024-07-12 12:06:53 -0500

    created all methods

---
+++ Module1Review/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Module1Review;
+
+class Program
+{
+    //Create method to get the area of a circle
+    public static double AreaOfCircle(double radius)
+    {
+        //uses .PI and .Pow for formula of a circle and then returns area
+        return Math.PI * Math.Pow(radius, 2);
+    }
+    
+    //Create method to get the area of a triangle
+    public static double AreaOfTriangle(double baseLength, double height)
+    {
+        return baseLength * height / 2;
+    }
+    
+    //Create method to get the area of a rectangle
+    public static double AreaOfRectangle(double length, double width)
+    {
+        return length * width;
+    }
+    
+    //Create method to get the area pf a square
+    public static double AreaOfSquare(double side)
+    {
+        //side^2
+        return Math.Pow(side, 2);
+    }
+    
+    //Used to call methods and use them
+    static void Main(string[] args)
+    {
+        
+    }
+}
