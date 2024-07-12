1a65f9e9aa42f02a050b42801323e3a538460b21
StewartBaker <bakrstewart16@gmail.com>
2024-07-12 12:07:34 -0500

    added switch

--- Module1Review/Program.cs
+++ Module1Review/Program.cs
@@ -31,6 +31,35 @@ class Program
     //Used to call methods and use them
     static void Main(string[] args)
     {
-        
+        //Used to figure out which shape the user wants the area of and stores it in variable
+        Console.WriteLine("What would you like the area of, Square, Rectangle, Triangle or Circle?");
+        var shape = Console.ReadLine();
+
+        //Puts user input in lowercase and then executes code depending on what matches, if none default output
+        switch (shape.ToLower())
+        {
+            case "circle":
+
+                break;
+            case "triangle":
+
+                break;
+            case "rectangle":
+
+                break;
+            case "square":
+
+                break;
+            //default output if user input does not match any shapes
+            default:
+                Console.WriteLine("Invalid Shape, Please pick a valid shape.");
+                break;
+        }
+        //Code for Triangle
+        Console.WriteLine("What is the Length of your Triangle?");
+        double baseLength = double.Parse(Console.ReadLine());
+        Console.WriteLine("What is the Height of your Triangle?");
+        double height = double.Parse(Console.ReadLine());
+        Console.WriteLine($"The are of a triangle with the baseLength of {baseLength} and height of {height} is {AreaOfTriangle(baseLength, height)}");
     }
 }
